e4c690c1e334ae781e5961827c4b1289c4816482
BezugliyVladimir <volodymyr6053@gmail.com>
2024-09-18 23:20:00 +0300

    added condition to disable plugin

--- Plugin/Store/HideDefaultStoreCodePlugin.php
+++ Plugin/Store/HideDefaultStoreCodePlugin.php
@@ -10,6 +10,13 @@ class HideDefaultStoreCodePlugin
 {
     public function afterIsUseStoreInUrl(Store $subject, bool $resultIsUseInUrl): bool
     {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $state =  $objectManager->get('Magento\Framework\App\State');
+
+        if ($state->getAreaCode() !== 'frontend') {
+            return false;
+        }
+
         if ($subject->getCode() !== Store::ADMIN_CODE && $subject->isDefault()) {
             return false;
         }
